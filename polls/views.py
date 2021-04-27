4b5322756851b68b395cf4d885bfce92550af47e
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-04-27 15:13:17 +0100

    set up main branch

---
+++ polls/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from polls.models import Question, Choice
+
+
+def index(request):
+
+    questions = Question.objects.all()
+    context = {
+        "questions": questions
+    }
+    return render(request, "polls/index.html", context)
